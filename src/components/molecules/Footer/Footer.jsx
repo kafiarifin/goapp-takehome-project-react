b52aef88790ba09c2e16e3d7f134e9ab049c44c7
Kafi Arifin <kafi.arifin.r.a@gmail.com>
2021-01-22 17:34:05 +0700

    Add Footer
    Footer Molecules Components

---
+++ src/components/molecules/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '@styles/Home.module.css';
+
+const Footer = () => {
+
+    return (
+        <footer className={styles.footer}>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src="/kafi-arifin.svg" alt="GoApp Logo" className={styles.logo} />
+                {' '}Powered by GoApp{' '}
+            </a>
+        </footer>
+    );
+}
+
+export default Footer;
